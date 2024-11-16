3e96b100b58dad457ed656419656815186d11640
Mohamed Alaa El-Din <moaalaa16@gmail.com>
2024-11-16 15:58:16 +0200

    Adding Default Configration

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {});
