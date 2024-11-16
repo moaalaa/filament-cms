03774da74b737d959755cafbc889cd2b51644dea
Mohamed Alaa El-Din <moaalaa16@gmail.com>
2024-11-16 16:27:20 +0200

    Adding Home Page

---
+++ app/Livewire/Pages/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+class Home extends Component
+{
+    #[Layout('layouts.app')]
+    public function render()
+    {
+        return view('livewire.pages.home');
+    }
+}
